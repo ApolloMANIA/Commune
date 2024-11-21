1030257df101e33c19c7174c852b8b9d14c4ae27
hrishabx <hrishabneupane@gmail.com>
2024-11-21 14:17:38 -0400

    uwu

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ const server = app.listen(port, ()=>{
     console.log(`server is running at http://localhost:${port}`);
 });
 
-mongoose.connect(databaseURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+// mongoose.connect(databaseURL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+  // })
+    // .then(() => console.log('Connected to MongoDB'))
+    // .catch((err) => console.error('MongoDB connection error:', err));
