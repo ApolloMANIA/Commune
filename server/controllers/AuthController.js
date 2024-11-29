c769db6f7f03dd16357b0d060ed02e83645b7b62
hrishabx <hrishabneupane@gmail.com>
2024-11-29 02:18:34 -0400

    server finished

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,10 +1,11 @@
-import User from "../models/UserModel";
-import { sign } from "jsonwebtoken";
+import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (email, userId) => {
-    return sign({email, userId}, process.env.JWT_KEY, {expiresIn: maxAge})
+    return jwt.sign({email, userId}, process.env.JWT_KEY, {expiresIn: maxAge,
+    });
 };
 
 export const signup = async (request, response, next) => {
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
 // for server communication
+// MVC
 app.use(
     cors({
         origin: [process.env.ORIGIN],
@@ -21,6 +23,8 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes)
 
 const server = app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
