20677e19455a66fec485e9880115b900a60d1563
Apollo <133866461+ApolloMANIA@users.noreply.github.com>
2024-12-02 01:42:50 -0400

    test

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoarrowBack } from "react-icons/io5";
 
+import { Avatar ,AvatarImage } from "@/components/ui/avatar";
+
 const Profile = () => {
   const { userInfo , setUserInfo } = useAppStore();
   const navigate = useNavigate();
@@ -22,12 +24,32 @@ const Profile = () => {
         <div>
           <IoarrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer"/>
           <div className="grid grid-cols-2">
-            <div className="h-full w32 md:w-48 md:h-48 relative flex items-center justify-center" onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}>
+            <div className="h-full w32 md:w-48 md:h-48 relative flex items-center justify-center" 
+            onMouseEnter={()=>setHovered(true)} 
+            onMouseLeave={()=>setHovered(false)}
+            >
+              <Avatar className='h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden'>
+              {
+                image ? (
+                <AvatarImage 
+                  src={image} 
+                  alt="profile" 
+                  className = "object-cover w-full h-full bg-black" 
+                  />
+                ) : (
+                  <div className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(selectedColor)} `}>
+                    {firstname
+                    ? firstname.split("").shift()
+                  : userInfo.email.split("").shift()}
+                  </div>
+              )}
+            </Avatar>
+            </div>
           </div>
         </div>
       </div>
-      Profile
-      <div>Email: {userInfo.email}</div>
+      {/* Profile
+      <div>Email: {userInfo.email}</div> */}
     </div>
   );
 };
